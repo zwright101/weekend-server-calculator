e9296c2196c381d4efab7492c262e7733dae115d
Zach Wright <zwright101@gmail.com>
2023-08-01 12:08:55 -0500

    functionality mostly works. when putting the numbers into the fields, i'm getting an alert saying something went wrong, but when hitting ok on the alert, the answer displays to the dom correctly. need to diagnose the error and get previous equations to display also

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,82 @@
+
 console.log('hello calculator peeps');
+
+document.querySelector('#firstInput').value = '';
+document.querySelector('#secondInput').value = '';
+
+let operator;
+
+function operatorButton(event){
+    operator = event.target.value;
+    console.log(operator);
+}
+
+function equalsButton(event){
+    let firstInput = document.querySelector('#firstInput').value;
+    let secondInput = document.querySelector('#secondInput').value;
+
+    console.log(firstInput, secondInput);
+    console.log(operator);
+
+
+    let numbers = {
+        number1: firstInput,
+        number2: secondInput,
+        operator: operator
+    }
+    console.log(numbers);
+    axios.post('/history' , numbers).then((response) => {
+        console.log(response);
+        results();
+        previousAnswers();
+    }).catch((error) => {
+        console.log();
+        alert('Something went Wrong!')
+    })
+}
+
+function results(){
+    axios.get('/results').then((response) => {
+        console.log(response);
+        let answerDiv = document.querySelector('#answer');
+        let answerFromServer = response.data;
+
+        for (let answer of answerFromServer){
+            answerDiv.innerHTML = `
+            <h2>Answer: ${answer}</h2>
+            `
+        }
+    })
+}
+
+function showAnswers(){
+    axios.get('/history').then((response) => {
+        console.log(response);
+        let numbersFromServer = response.data;
+        let answerDiv = document.querySelector('#answer')
+
+        answerDiv.innerHTML += `
+        <ul>
+            <li>   
+                <p>
+                ${numbersFromServer[numbersFromServer.length - 1]}
+                </p>
+            </li>    
+        </ul>
+        <button onClick="deleteAnswer()">Delete</button>
+    `
+    }).catch((error) => {
+        console.log(error);
+        alert('Something went Wrong!')
+    })
+}
+
+function deleteAnswer(index){
+    axios.delete(`/history/${index}`).then((response) => {
+        console.log(response);
+        showAnswers();
+    }).catch((error) => {
+        console.log(error);
+        alert('Something went Wrong!')
+    })
+}
--- server/server.js
+++ server/server.js
@@ -1,14 +1,46 @@
 let express = require('express');
-const problemToSolve = require('./problem');
 let app = express();
 const port = 5001;
 
 app.use(express.json());
 
-
-
 app.use(express.static('server/public'));
 
 app.listen(port, function(){
     console.log('listening on port' , port);
+})
+
+let results = [];
+
+let resultHistory = [];
+
+app.get('/results' , (req, res) => {
+    console.log('GET request for /results');
+    res.send(results);
+})
+
+app.get('/history' , (req, res) => {
+    console.log('GET request for results history');
+    res.send(resultHistory);
+})
+
+app.post('/history' , (req, res) => {
+    console.log('POST request for /history');
+    let numbersToAdd = req.body;
+    let newResult;
+
+    if (numbersToAdd.operator === '+') {
+        newResult = Number(numbersToAdd.number1) + Number(numbersToAdd.number2);
+    }else if (numbersToAdd.operator === '-'){
+        newResult = Number(numbersToAdd.number1) - Number(numbersToAdd.number2);
+    }else if (numbersToAdd.operator === '*'){
+        newResult = Number(numbersToAdd.number1) * Number(numbersToAdd.number2);
+    }else if (numbersToAdd.operator === '/'){
+        newResult = Number(numbersToAdd.number1) / Number(numbersToAdd.number2);
+    }
+
+    let calculation = `${numbersToAdd.number1} ${numbersToAdd.operator} ${numbersToAdd.number2} = ${newResult}`
+    resultHistory.push(calculation);
+    results.push(newResult);
+    res.sendStatus(201);
 })
